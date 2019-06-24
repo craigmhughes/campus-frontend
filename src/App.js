b137282eb851acbaf0057d06eb724f437bdc0e86
Craig Hughes <hghscraig@gmail.com>
2019-06-24 17:08:04 +0100

    Add sign in check

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import LoginScreen from './components/LoginScreen';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      signed_in: false,
+    }
+
+    this.check_signin();
+  }
+
+  check_signin(){
+
+    let subdomain = window.location.href.split(window.location.host)[1];
+
+    if(this.state.signed_in == false && subdomain !== "/signin"){
+      window.location.href = "/signin";
+    }
+  }
+
+  render(){
+
+    return(
+      <div className="App">
+        <Router>
+
+          <Route path="/signin" exact component={LoginScreen} />
+        
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;

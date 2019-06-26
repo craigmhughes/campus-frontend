679b54dcb97a5f0285e5c4066abc8b45750c0417
Craig Hughes <hghscraig@gmail.com>
2019-06-26 16:49:49 +0100

    Added button style fix

--- src/components/Auth/AuthScreen.js
+++ src/components/Auth/AuthScreen.js
@@ -9,23 +9,72 @@ class AuthScreen extends React.Component {
     this.state = {
       auth_screen: 0
     }
+
+    this.footer_el = React.createRef();
+  }
+
+  switch_authscreen(screen){
+
+    this.setState({
+      auth_screen: screen
+    }, ()=>{
+
+      let cc_buttons = this.footer_el.current.childNodes;
+
+      switch(this.state.auth_screen){
+        case 0:
+          this.render_menu();
+          break;
+        case 1:
+          this.render_signup();
+          break;
+        case 2:
+          this.render_signin();
+          break;
+      }
+    });
+
+  }
+
+  render_menu(){
+    
+  }
+
+  render_signup(){
+
+  }
+
+  render_signin(){
+
   }
 
   render(){
     return(
-      <div className="LoginScreen">
+      <div className="AuthScreen">
+
+          <i className="far fa-chevron-left" id="back-icon" onClick={()=>{this.switch_authscreen(0)}}/>
 
           <div className="header">
-            <img src="./images/logo.svg" className="logo logo-light"/>
+            {/* <img src="./images/logo.svg" className="logo logo-light"/> */}
             <p className="logo-text">campuschat</p>
           </div>
           <div className="body">
+            <div className="login-form">
+              <h1>Login</h1>
+              <input name="login-mail" placeholder="E-Mail Address" type="email"></input>
+              <input name="login-pass" placeholder="Password" type="password"></input>
+            </div>
 
+            <div className="signup-form">
+              <h1>Signup</h1>
+              <input name="register-mail" placeholder="E-Mail Address" type="email"></input>
+              <input name="register-pass" placeholder="Password" type="password"></input>
+            </div>
           </div>
-          <div className="footer">
+          <div className="footer" ref={this.footer_el}>
             
-              <a className="cc-button light-secondary">Sign Up</a>
-              <a className="cc-button primary right">Log In</a>
+              <button className="btn btn-secondary btn-light btn-left" onClick={()=>{this.switch_authscreen(1)}}>Sign Up</button>
+              <button className="btn btn-primary btn-light" onClick={()=>{this.switch_authscreen(2)}}>Log In</button>
             
           </div>
       </div>
